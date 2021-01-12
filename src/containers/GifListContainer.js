6697f13c873af1e5a309de2e403d44e78b8622b8
JanaKhanji <janakhanji98@gmail.com>
2021-01-12 19:51:59 +0000

    wip

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
+
+class GifListContainer extends Component {
+
+  state = {
+    gifs: []
+  }
+render(){
+    return (<div></div>)
+}
+}
